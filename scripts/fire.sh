c4af8f1174e9cdb776afc6116f8f363b60849c37
gfw-breaker <lixiaoming0513@gmail.com>
2022-11-20 08:49:54 +0800

    同步于: 11月20日8时49分

--- scripts/fire.sh
+++ scripts/fire.sh
@@ -65,6 +65,9 @@ while read line; do
 	link=$(echo $line | cut -d',' -f1)
 	title=$(echo $line | cut -d',' -f2)
 	echo -e "#### [$title](https://github.com/gfw-breaker$link)" >> $hot_page
+	md_path=$(echo "/root/repos$line" | sed 's#/blob/master##')
+	echo $md_path
+	touch $md_path
 done < /root/page_count/banned-news3.hot
 
 echo 'generate'
